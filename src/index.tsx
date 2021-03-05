c2d4bfb9e59e1d7bf08a3e88b652b25ed333ed89
Fernando Salas <fernando.salas@deliveryhero.com>
2021-03-05 12:27:31 +0100

    Test pre-commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Initial TS React App {''}</h1>;
+const App = () => <h1>Testing pre-commit hook</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
