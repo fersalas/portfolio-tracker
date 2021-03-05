49346ce92ceb0519fe7e3975063dc49ca0a531d7
Fernando Salas <fernando.salas@deliveryhero.com>
2021-03-05 12:07:46 +0100

    Setup prettier and lint script

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => (
-  <h1>
-      Initial TS React App {" "}
-      {new Date().toLocaleDateString()}
-  </h1>
-);
+const App = () => <h1>Initial TS React App {''}</h1>;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
